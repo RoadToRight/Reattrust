b4ddf8791cc382ddd990b3638915f18423525c6a
Faizan <faizantfl@gmail.com>
2026-01-07 01:00:20 +0500

    complete about section

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -10,7 +10,7 @@ export const About = () => {
 
                 
             </div>
-            <div className="col-2">
+            <div class="col-2">
                 <h1 class="about-heading">
                      Not Your Average Realtor
                 </h1>
@@ -20,11 +20,28 @@ export const About = () => {
                 <button class="about-btn">
                 Learn More
                 </button>
-                <div className="about-counter">
+            </div>
 
+            <div className="col-3">
+                <div className="about-counter">
+                <h4 class="counter-heading" > 10k </h4>
+                <p class="counter-paragraph"> Total Clients</p>
                 </div>
 
+                <div className="about-counter">
+                <h4 class="counter-heading" > 1800+</h4>
+                <p class="counter-paragraph"> Total Clients</p>
+                </div>
+    
+
+                <div className="about-counter">
+                <h4 class="counter-heading" > 3,420,90+</h4>
+                <p class="counter-paragraph"> Total Clients</p>
+                </div>
+
+
             </div>
+
      
         </section>
         
@@ -39,14 +56,40 @@ const AboutSec = styled.section
    margin:0 auto;
   padding:80px 20px;
   display:flex;
+  flex-wrap:wrap;
   flex-direction:row;
   gap:40px;
-  justify-content:center;
-  align-item:center; 
+  justify-content:end;
+  
 
 }
 .col-1 , .col-2 {
-  width:50%;
+  width:48%;
+
+}
+.col-3 {
+width:780px;
+height:200px;
+background-color:#181C21;
+padding:55px 80px;
+display: flex;
+justify-content: center;
+align-items: center;
+gap: 100px;
+position:relative;
+margin-top: -135px;
+
+}  
+
+.col-3::before {
+content: "";
+position: absolute;
+background: url(/memphics_003.png) no-repeat center center / contain !important;
+z-index: -1;
+width: 138px;
+min-height: 138px;
+bottom: -30px;
+right: -55px;
 
 }
 .col-2 {
@@ -75,17 +118,32 @@ font-weight: 600;
 }
 
 .about-paragraph {
-    font-size: 16px;
+    font-size: 18px;
     font-weight: 400 !important;
     font-family: "Poppins", sans-serif !important;
     line-height: 30px;
-    max-width: 481px;
+    max-width: 560px;
 }
 
+
+// column 3 inner section  
 .about-counter {
-   min-height:250px;
-   padding:60px 80px;
-   background-color:#000;
-   
-   }
+display: flex;
+gap: 20px;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+}
+.counter-heading {
+font-size: 32px;
+color: #BAA360;
+font-family: "Poppins", sans-serif !important;
+line-height: 130%;
+}
+.counter-paragraph {
+color: #BAA360;
+font-size: 18px;
+font-family: "Poppins", sans-serif !important;
+font-weight: 600;
+}
 `
