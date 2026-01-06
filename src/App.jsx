bde61e2f1c3a0a6e47d734d031b72e4c6acd5a85
Syed Sameer Hussain Shah <syedsameershah0786@gmail.com>
2026-01-06 13:37:34 -0500

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Nav from './Components/Nav'
 import Hero from './Components/Hero'
+import Footer from './Components/Footer'
 
 const App = () => {
   return (
     <div>
       <Nav />
       <Hero />
+      <Footer />
     </div>
   )
 }
